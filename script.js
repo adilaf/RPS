c347b5918e5d35e22682adf76fca4fa391bbbbe9
adilaf <100770370+adilaf@users.noreply.github.com>
2023-03-13 17:10:47 +0800

    moved script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function say(myMessage){
+    var msg = new SpeechSynthesisUtterance(myMessage);
+    window.speechSynthesis.speak(msg);
+  }
+
+function play(user) {
+    let result = "";
+    if (user === 'rock') {
+        result = "tie";
+    }
+    if (user === 'paper') {
+        result = "win";
+    }
+    if (user === 'scissors') {
+        result = "lose";
+    }
+    var theMessage = ("Computer chose rock, you " + result + "!"); 
+    alert(theMessage);
+    say(theMessage);
+}
+
+
 
