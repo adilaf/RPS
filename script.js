41fc9a02467052b696c687010b95f2afcc49a62e
adilaf <100770370+adilaf@users.noreply.github.com>
2023-03-13 16:51:49 +0800

    script added

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-function play(user) {
-    let result = "";
-    if (user === 'rock') {
-        result = "tie";
-    }
-    if (user === 'paper') {
-        result = "win";
-    }
-    if (user === 'scissors') {
-        result = "lose";
-    }
-    alert("Computer chose rock, you " + result + "!");
-}
